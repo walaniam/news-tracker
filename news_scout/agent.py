eeaf4e14d6cae08c5f0c62964475fad540b0dddf
walaniam <mariusz.walania@gmail.com>
2026-04-14 13:56:24 +0200

    Change user agent

--- news_scout/agent.py
+++ news_scout/agent.py
@@ -22,9 +22,16 @@ logger = logging.getLogger(__name__)
 
 _REQUEST_HEADERS = {
     "User-Agent": (
-        "Mozilla/5.0 (compatible; NewsScout/1.0; "
-        "+https://github.com/walaniam/news-tracker)"
-    )
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/124.0.0.0 Safari/537.36"
+    ),
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "DNT": "1",
 }
 _REQUEST_TIMEOUT = 15  # seconds
 _INTER_REQUEST_DELAY = 0.5  # seconds between site requests
